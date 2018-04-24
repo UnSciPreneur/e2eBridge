97a63ddc9785d04bbb283b093cb64fb3afd39b6c
Andreas Bogner <andreas.bogner@siemens.com>
2018-04-24 14:17:22 +0200

    removing obsolete script

--- storeAddresses.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# possible values for class are
-#  exchange
-#
-
-curl -XPUT 'http://localhost:9200/ethereum/address/0x2910543af39aba0cd09dbb2d50200b3e800a63d2' -d '{"comment" : "Kraken", "class" : "exchange"}';
-curl -XPUT 'http://localhost:9200/ethereum/address/0x32be343b94f860124dc4fee278fdcbd38c102d88' -d '{"comment" : "Poloniex Wallet", "class" : "exchange"}';
-curl -XPUT 'http://localhost:9200/ethereum/address/0xb794f5ea0ba39494ce839613fffba74279579268' -d '{"comment" : "Poloniex ColdWallet", "class" : "exchange"}';
